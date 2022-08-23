46bfc93ae3be9792e4e08dcf8a7b3e2a0924315b
Lukáš Tinkl <lukas@kde.org>
2022-08-23 13:22:20 +0200

    complete neutral colors palette

--- imports/StatusQ/Core/Theme/basepalette.cpp
+++ imports/StatusQ/Core/Theme/basepalette.cpp
@@ -4,12 +4,18 @@ namespace {
 constexpr QColor kNeutralSolidColor{100, 112, 132}; // #647084
 
 // common neutral colors, for both themes
-constexpr std::array<Palette::ColorPair, 4> kNeutralColors = {{
+constexpr std::array<Palette::ColorPair, 11> kNeutralColors = {{
     {Palette::Shade95, {9, 16, 28}},  // #09101C
     {Palette::Shade90, {13, 22, 37}}, // #0D1625
+    {Palette::Shade80, {19, 29, 47}}, // #131D2F
+    {Palette::Shade70, {25, 36, 56}}, // #192438
     {Palette::Shade60, {48, 61, 85}}, // #303D55
     {Palette::Shade50, kNeutralSolidColor},
-    // TODO
+    {Palette::Shade40, {161, 171, 189}}, // #A1ABBD
+    {Palette::Shade30, {231, 234, 238}}, // #E7EAEE
+    {Palette::Shade20, {236, 238, 241}}, // #ECEEF1
+    {Palette::Shade10, {240, 242, 245}}, // #F0F2F5
+    {Palette::Shade5, {245, 246, 248}}, // #F5F6F8
 }};
 } // namespace
 
